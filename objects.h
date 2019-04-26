40e247e2b5f3cbbf6b0004a8c39ca6f3c6db9476
Bradley Robinson <sc18br@leeds.ac.uk>
2019-04-26 21:07:11 +0100

    rename pellets to objects

---
+++ objects.h
@@ -0,0 +1,8 @@
+//void initobjects(object *head, SDL_Texture *texture);
+void drawObjects(node *head);
+void addObject(node *head, object new);
+void destroyobject(node **head, int index);
+bool checkCollisionWithPlayer(node *head, object collider);
+void checkCollisionWithY(node *head);
+void updateObjectPos(node *head);
+void destroyAll(node *head);
